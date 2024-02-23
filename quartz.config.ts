438d788a03ba43cc9aa76739710b2bfe27776f43
Kupier System <princess.vixin@kupier.gay>
2024-02-24 01:35:50 +1100

    Quartz sync: Feb 24, 2024, 1:35 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ndis.lgbt",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
