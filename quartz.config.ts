ba24445885ba46d36f721ed542bfd1b848301c26
princessvixin <princess.vixin@kupier.gay>
2024-02-25 15:05:34 +1100

    theme

--- quartz.config.ts
+++ quartz.config.ts
@@ -61,7 +61,7 @@ const config: QuartzConfig = {
         },
         // set this to 'true' to use the background color of the Shikiji theme
         // if set to 'false', will use Quartz theme colors for background
-        keepBackground: false,
+        keepBackground: true,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
