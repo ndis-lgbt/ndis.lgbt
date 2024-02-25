67f9ed3005c2e4018278300ffa6a9e3e44b6cbff
princessvixin <princess.vixin@kupier.gay>
2024-02-26 00:04:04 +1100

    removed broken trans flag

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🏳️‍🌈 Queer NDIS Guide 🏳️‍⚧️",
+    pageTitle: "🏳️‍🌈 Queer NDIS Guide",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
